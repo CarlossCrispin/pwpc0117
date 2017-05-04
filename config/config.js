b0d3b7b2fab7af7489670b5aadd18b89a15c25de
CarlossCrispin <carlos.crispin.cc@gmail.com>
2017-05-04 12:09:29 -0500

    cambaindo puerto

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ module.exports = {
    //"IP":'127.0.0.1' || process.env.IP,
     //"IP": process.env.IP || '127.0.0.1',
    "IP": process.env.IP || '0.0.0.0',
+   "PORT":process.env.PORT || '3000',
     "color_theme": {
         "info":"rainbow",
         "data":"green",
