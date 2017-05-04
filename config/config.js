2d092e1a46deafd0c69373bed7fd4f105f501206
CarlossCrispin <carlos.crispin.cc@gmail.com>
2017-05-04 11:57:12 -0500

    Desplegando app

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 //exporta el objeto module.exports
 module.exports = {
-   // "IP":'127.0.0.1' || process.env.IP,
-    "IP": process.env.IP || '0.0.0.0',
-    "PORT":process.env.PORT || '3000',
+   "IP":'127.0.0.1' || process.env.IP,
+    //"IP": process.env.IP || '127.0.0.1',
+   "IP": process.env.IP || '0.0.0.0',
     "color_theme": {
         "info":"rainbow",
         "data":"green",
@@ -10,5 +10,5 @@ module.exports = {
         "warning":"yellow"
     },//estatico
     "STATIC_PATCH":"./static",
-   // "dbStringConnection":process.env.DB || "mongodb://localhost:27017/invetarios"
+    "dbStringConnection":process.env.DB || "mongodb://localhost:27017/bodega"
 };
