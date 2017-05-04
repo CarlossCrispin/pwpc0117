a1af847f0f3f01d04f94c0479ae93f633ddd9a1c
CarlossCrispin <carlos.crispin.cc@gmail.com>
2017-05-04 12:07:29 -0500

    cambaindo puerto

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //exporta el objeto module.exports
 module.exports = {
-   "IP":'127.0.0.1' || process.env.IP,
+   //"IP":'127.0.0.1' || process.env.IP,
     //"IP": process.env.IP || '127.0.0.1',
    "IP": process.env.IP || '0.0.0.0',
     "color_theme": {
