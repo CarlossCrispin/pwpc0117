3aa78fcdd17930f502b2b673ccad678cfab7af97
CarlossCrispin <carlos.crispin.cc@gmail.com>
2017-05-08 14:32:28 -0500

    modificando JSON (npm)

--- config/config.js
+++ config/config.js
@@ -2,8 +2,10 @@
 module.exports = {
    //"IP":'127.0.0.1' || process.env.IP,
     //"IP": process.env.IP || '127.0.0.1',
-   "IP": process.env.IP || '0.0.0.0',
-   "PORT":process.env.PORT || '3000',
+  // "IP": process.env.IP || '0.0.0.0',
+  // "PORT":process.env.PORT || '3000',
+      "IP": process.env.IP, 
+   "PORT":process.env.PORT,
     "color_theme": {
         "info":"rainbow",
         "data":"green",
