e837f4ceb6b1b171bfc39dda4a0dbe843b9604bb
CarlossCrispin <carlos.crispin.cc@gmail.com>
2017-05-08 19:29:37 -0500

    modificando cliente  en handlers

--- internals/handlers.js
+++ internals/handlers.js
@@ -1,5 +1,5 @@
 var staticServer =require("./static-server");
-//var mongo = require('mongodb').MongoClient;
+var mongo = require('mongodb').MongoClient;
 var url = 'mongodb://127.0.0.1:27017/Condominio'
 //var url = 'mongodb://carlos:crispin@ds131511.mlab.com:31511/condominio'
 
